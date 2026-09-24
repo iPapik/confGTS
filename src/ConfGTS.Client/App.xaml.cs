4999023c1c41e11f820e8ff8977722f2c50e7499
iPapik <164984649+iPapik@users.noreply.github.com>
2026-09-24 17:22:40 +0500

    Add src/ConfGTS.Client/App.xaml.cs

---
+++ src/ConfGTS.Client/App.xaml.cs
@@ -0,0 +1,19 @@
+using Microsoft.UI.Xaml;
+
+namespace ConfGTS.Client;
+
+public partial class App : Application
+{
+    public static MainWindow? MainWindowInstance { get; private set; }
+
+    public App()
+    {
+        InitializeComponent();
+    }
+
+    protected override void OnLaunched(LaunchActivatedEventArgs args)
+    {
+        MainWindowInstance = new MainWindow();
+        MainWindowInstance.Activate();
+    }
+}
